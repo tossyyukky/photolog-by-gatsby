02be43998ef5d5800dbb585044285a3cee69e06e
Toshiyuki Ihara <snowsphere.burned@gmail.com>
2020-11-07 22:14:15 +0900

    Add: GA and Sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "./entry.module.scss"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
